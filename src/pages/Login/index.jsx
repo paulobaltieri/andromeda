cf3fb5bbe4246672065c4c962be4027b99534e29
paulobaltieri <baltieri_paulo@hotmail.com>
2026-04-19 14:52:26 -0300

    Ajustes tela de login

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,22 +1,9 @@
-import { Card, CardContent, Box } from "@mui/material"
+
 
 export default function Login() {
     return (
-        <Box sx={{
-            display: "flex",
-            justifyContent: "center",
-            mt: 10,
-        }}>
-            <Card sx={{
-                width: "440px",
-                height: "500px",
-                backgroundColor: "red",
-                borderRadius: "15px",
-            }}>
-                <CardContent>
-                    <h1>Login</h1>
-                </CardContent>
-            </Card>
-        </Box>
-    )
+        <div>
+            <h1>Login</h1>
+        </div>
+    );
 }
